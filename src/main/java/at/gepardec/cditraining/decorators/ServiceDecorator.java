c4cfe6b392a9b320fc2b84ecded7d0e62ee97beb
fhochleitner <felix.hochleitner@outlook.com>
2021-03-22 11:44:57 +0100

    Concepts/decorators - task 1

--- src/main/java/at/gepardec/cditraining/decorators/ServiceDecorator.java
+++ src/main/java/at/gepardec/cditraining/decorators/ServiceDecorator.java
@@ -2,9 +2,14 @@ package at.gepardec.cditraining.decorators;
 
 import org.slf4j.Logger;
 
+import javax.annotation.Priority;
+import javax.decorator.Decorator;
 import javax.decorator.Delegate;
 import javax.inject.Inject;
+import javax.interceptor.Interceptor;
 
+@Decorator
+@Priority(Interceptor.Priority.APPLICATION)
 public abstract class ServiceDecorator implements ServiceApi {
 
     @Inject
